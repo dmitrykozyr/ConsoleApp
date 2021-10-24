8eeae623d09b11a1df130d099ffff63c8c8b8f3f
dmitrykozyr <dmitry.kozyr.ua@gmail.com>
2021-10-24 12:04:34 +0300

    --

---
+++ EntityFrameworkEdu/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFrameworkEdu
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Навигационное св-во
+        public User User { get; set; }
+    }
+}
