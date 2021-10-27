f13f0900a559af114aefa425a90549e67fecbb19
dmitrykozyr <dmitry.kozyr.ua@gmail.com>
2021-10-27 08:28:08 +0300

    --

--- EntityFrameworkEdu/Player.cs
+++ EntityFrameworkEdu/Player.cs
@@ -3,8 +3,7 @@
     public class Player
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        // Навигационное св-во
-        public User User { get; set; }
+        public string Name { get; set; }        
+        public User User { get; set; }  // Навигационное свойствово
     }
 }
