a75483f0cb02d77feca912fe68e81071c10f31f8
dmitrykozyr <dmitry.kozyr.ua@gmail.com>
2022-01-03 11:38:08 +0200

    --

---
+++ ECommerce.Api.Products.Tests/ProductsServcieTest.cs
@@ -0,0 +1,95 @@
+using AutoMapper;
+using ECommerce.Api.Products.Db;
+using ECommerce.Api.Products.Profiles;
+using ECommerce.Api.Products.Providers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+// Этот проект создан на основе xUnit .Net Core
+// Добавляем ссылку на проект Products, т.к. будем его тестировать
+
+// Чтобы автоматизировать запуск тестов, создадим проект на https://azure.microsoft.com/en-us/services/devops/
+// https://www.linkedin.com/learning/azure-microservices-with-dot-net-core-for-developers/creating-an-azure-devops-project?autoAdvance=true&autoSkip=true&autoplay=true&resume=false&u=2113185
+
+namespace ECommerce.Api.Products.Tests
+{
+    // Для запуска тестов ПКМ на названии класса -> Run Tests
+    public class ProductsServcieTest
+    {
+        [Fact] // Часть фреймворка тестирвоания xUnit, обозначает юнит тест
+        public async Task GetProductsReturnsAllProducts()
+        {
+            // Для тестов используем InMemoryDatabase, чтобы не влиять на реальные данные
+            var options = new DbContextOptionsBuilder<ProductsDbContext>().UseInMemoryDatabase(nameof(GetProductsReturnsAllProducts)).Options;
+            var dbContext = new ProductsDbContext(options );
+            CreateProducts(dbContext);
+
+            var productProfile = new ProductProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
+            var mapper = new Mapper(configuration);
+
+            var productsProvider = new ProductsProvider(dbContext, null, mapper);
+            var products = await productsProvider.GetProductsAsync();
+            Assert.True(products.IsSuccess);
+            Assert.True(products.Products.Any());
+            Assert.Null(products.ErrorMessage);
+        }
+
+        [Fact]
+        public async Task GetProductsReturnsProductUsingValidId()
+        {
+            var options = new DbContextOptionsBuilder<ProductsDbContext>().UseInMemoryDatabase(nameof(GetProductsReturnsProductUsingValidId)).Options;
+            var dbContext = new ProductsDbContext(options);
+            CreateProducts(dbContext);
+
+            var productProfile = new ProductProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
+            var mapper = new Mapper(configuration);
+
+            int productId = 1;
+            var productsProvider = new ProductsProvider(dbContext, null, mapper);
+            var product = await productsProvider.GetProductAsync(productId);
+            Assert.True(product.IsSuccess);
+            Assert.NotNull(product.Product);
+            Assert.True(product.Product.Id == productId);
+            Assert.Null(product.ErrorMessage);
+        }
+
+        [Fact]
+        public async Task GetProductsReturnsProductUsingInvalidId()
+        {
+            var options = new DbContextOptionsBuilder<ProductsDbContext>().UseInMemoryDatabase(nameof(GetProductsReturnsProductUsingInvalidId)).Options;
+            var dbContext = new ProductsDbContext(options);
+            CreateProducts(dbContext);
+
+            var productProfile = new ProductProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
+            var mapper = new Mapper(configuration);
+
+            int productId = -1;
+            var productsProvider = new ProductsProvider(dbContext, null, mapper);
+            var product = await productsProvider.GetProductAsync(productId);
+            Assert.False(product.IsSuccess);
+            Assert.Null(product.Product);
+            Assert.NotNull(product.ErrorMessage);
+        }
+
+        private void CreateProducts(ProductsDbContext dbContext)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                dbContext.Products.Add(new Product()
+                {
+                    Id = i,
+                    Name = Guid.NewGuid().ToString(),
+                    Inventory = i + 10,
+                    Price = (decimal)(i * 3.14)
+                });
+            }
+            dbContext.SaveChanges();
+        }
+    }
+}
