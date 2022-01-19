699b4bb3930a459b8e9c0a51312bffeb5c4078ec
dmitrykozyr <dmitry.kozyr.ua@gmail.com>
2022-01-19 11:24:44 +0200

    --

---
+++ WebApiNetCore/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// Создание контроллера: на папке Controllers ПКМ -> Add Controller -> Api Controller - Empty
+namespace WebApiNetCore.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ WebApiNetCore/Readme.txt
@@ -0,0 +1,2 @@
+﻿Проект создан на основе ASP.NETY Core Web Application -> API
+https://www.linkedin.com/learning/building-web-apis-with-asp-dot-net-core-3/hello-world-api-style?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=2113185
