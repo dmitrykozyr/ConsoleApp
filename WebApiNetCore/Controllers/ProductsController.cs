b21a73e8c1eb1e933150d7d48404b9cd65231024
dmitrykozyr <dmitry.kozyr.ua@gmail.com>
2022-01-22 14:15:55 +0200

    --

--- WebApiNetCore/Controllers/ProductsController.cs
+++ WebApiNetCore/Controllers/ProductsController.cs
@@ -2,7 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApiNetCore.Classes;
 using WebApiNetCore.DatabaseContext;
+using WebApiNetCore.Models;
 
 // Создание контроллера: на папке Controllers ПКМ -> Add Controller -> Api Controller - Empty
 namespace WebApiNetCore.Controllers
@@ -19,11 +21,44 @@ namespace WebApiNetCore.Controllers
             _context.Database.EnsureCreated();
         }
 
-
         [HttpGet]
-        public async Task<IActionResult> GetAllProducts()
+        // Пагинация    https://localhost:44345/Products?size=10&page=2,
+        // Фильтры      https://localhost:44345/Products?size=10&page=2&minPrice=1&maxPrice=20
+        // Сортировка   https://localhost:44345/Products?sortBy=Price&sortOrder=desc
+        // [FromQuery] QueryParameters позволяет получать параметры из URL
+        public async Task<IActionResult> GetAllProducts([FromQuery] ProductQueryParameters queryParameters)
         {
-            return Ok(await _context.Products.ToArrayAsync());
+            IQueryable<Product> products = _context.Products;
+
+            // Фильтры
+            if(queryParameters.MinPrice != null && queryParameters.MaxPrice != null)
+            {
+                products = products.Where(p => p.Price >= queryParameters.MinPrice.Value && p.Price <= queryParameters.MaxPrice.Value);
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.Sku))
+            {
+                products = products.Where(p => p.Sku == queryParameters.Sku);
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.Name))
+            {
+                products = products.Where(p => p.Name.ToLower().Contains(queryParameters.Name.ToLower()));
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.SortBy))
+            {
+                if (typeof(Product).GetProperty(queryParameters.SortBy) != null)
+                {
+                    products = products.OrderByCustom(queryParameters.SortBy, queryParameters.SortOrder);
+                }
+            }
+
+            // Пагинация
+            products = products.Skip(queryParameters.Size * (queryParameters.Page - 1))
+                               .Take(queryParameters.Size);
+
+            return Ok(await products.ToArrayAsync());
         }
 
         [HttpGet("{id:int}")]
--- WebApiNetCore/Readme.txt
+++ WebApiNetCore/Readme.txt
@@ -1,2 +1,7 @@
 ﻿Проект создан на основе ASP.NETY Core Web Application -> API
 https://www.linkedin.com/learning/building-web-apis-with-asp-dot-net-core-3/hello-world-api-style?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=2113185
+
+Темы:
+- Роутинг
+- Пагинация
+- Фильтрация
