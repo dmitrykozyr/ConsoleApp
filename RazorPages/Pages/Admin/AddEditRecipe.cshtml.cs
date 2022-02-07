502c94f1592a75c6e74d34caa5cf66d4657addc4
dmitrykozyr <dmitry.kozyr.ua@gmail.com>
2022-02-07 21:56:18 +0200

    --

--- RazorPages/Pages/Admin/AddEditRecipe.cshtml.cs
+++ RazorPages/Pages/Admin/AddEditRecipe.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 using TopsyTurvyCakes.Models;
 
 namespace RazorPages.Pages.Admin
@@ -14,10 +15,34 @@ namespace RazorPages.Pages.Admin
             get { return Id == null; }
         }
 
+        [BindProperty]
         public Recipe Recipe { get; set; }
 
-        public void OnGet()
+        private readonly IRecipesService _recipesService;
+
+        public AddEditRecipeModel(IRecipesService recipesService)
         {
+            _recipesService = recipesService;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Recipe = await _recipesService.FindAsync(Id.GetValueOrDefault()) ?? new Recipe();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Recipe.Id = Id.GetValueOrDefault();
+            await _recipesService.SaveAsync(Recipe);
+
+            // После сохранения рецепта произойдет редирект на страницу с рецептом
+            return RedirectToPage("/Recipe", new { id = Recipe.Id });
+        }
+
+        public async Task<IActionResult> OnPostDelete()
+        {
+            await _recipesService.DeleteAsync(Id.Value);
+            return RedirectToPage("/Index");
         }
     }
 }
