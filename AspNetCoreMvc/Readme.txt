69153354bb9e96576220b23d4501d391290c5c42
dmitrykozyr <dmitry.kozyr.ua@gmail.com>
2022-02-10 21:31:07 +0200

    --

---
+++ AspNetCoreMvc/Readme.txt
@@ -0,0 +1,3 @@
+﻿Создано на основе ASP .Net Core Web Application -> Empty
+
+https://www.linkedin.com/learning/learning-asp-dot-net-core-mvc/create-a-new-project-2?autoAdvance=true&autoSkip=false&autoplay=true&resume=false&u=2113185
---
+++ AspNetCoreMvc/Startup.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetCoreMvc
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("2");
+                await next();
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("1");
+            });
+
+        }
+    }
+}
