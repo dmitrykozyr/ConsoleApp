7d160ee29db47497fbb03df25b7b56b879b908a6
dmitrykozyr <dmitry.kozyr.ua@gmail.com>
2022-02-11 09:15:36 +0200

    --

--- AspNetCoreMvc/Startup.cs
+++ AspNetCoreMvc/Startup.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,28 +8,43 @@ namespace AspNetCoreMvc
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseExceptionHandler("/error.html");
+
+            // ПКМ на проекте -> Properties -> Debug
+            // Если для переменной окружения ASPNETCORE_ENVIRONMENT вместо 'Development'
+            // прописать 'Production', то код ниже не будет выполняться
+            // EnableDeveloperExceptions - это тоже переменная окружения, которую там-же добавили
+            //if (_configuration["EnableDeveloperExceptions"] == "True")
+
+            // Или можно добавить переменную в конфиг в appsettings.json
+            if (_configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions"))
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            // Вывести ошибку, если адрес страницы содержит слово invalid
             app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("2");
+                if (context.Request.Path.Value.Contains("invalid"))
+                    throw new System.Exception("ERROR!");
+
                 await next();
             });
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("1");
-            });
-
+            app.UseFileServer();
         }
     }
 }
