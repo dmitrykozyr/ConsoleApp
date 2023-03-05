d395896d5bde391bad04a6e03d29ab3ea66a1787
Dima Kozyr <dmitry.kozyr.ua@gmail.com>
2023-03-05 12:47:07 +0300

    --

--- Projects/Architectures/UI Patterns/MVVM/obj/Debug/net6.0-windows/App.g.i.cs
+++ Projects/Architectures/UI Patterns/MVVM/obj/Debug/net6.0-windows/App.g.i.cs
@@ -53,7 +53,7 @@ namespace MVVM {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MVVM;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MVVM;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
