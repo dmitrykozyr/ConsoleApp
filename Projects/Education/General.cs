6a8feee24fc1fd3831249f5aff2850da7fcfd6ed
MYPC\dmitr <dmitry.kozyr.ua@gmail.com>
2024-08-11 08:50:03 +0300

    --

--- Projects/Education/General.cs
+++ Projects/Education/General.cs
@@ -146,6 +146,57 @@ namespace SharpEdu
             */
         }
 
+        class Switch_
+        {
+            public string MyProperty { get; set; }
+
+            static void Main_()
+            {
+                object a = "5"; // Тип object 
+
+                // v1
+                switch (a)
+                {
+                    case string s: // Преобразование object в string
+                        // ...
+                        break;
+                    case Switch_ sw when sw.MyProperty == "1":
+                        // Если на вход передается объект класса Switch_
+                        // со значением переменной MyProperty == "1"
+                        break;
+                }
+
+                // v2
+                var switchV2 = a switch
+                {
+                    "1" => "Вернуть результат 1",
+                    "2" => "Вернуть результат 2",
+                    "3" or "4" => "Вернуть результат 3",
+                    _ => throw new NotImplementedException() // default
+                };
+
+                // v3 с кортежами
+                var switchV3 = (a:1, b:"11") switch
+                {
+                    (1, "11") => "Вернуть результат 1",
+                    (2, "11") or (2, "22") => "Вернуть результат 2",
+                    (3, "33") => "Вернуть результат 3",
+                    (_, "44") => "Вернуть результат 4", // первое значение пропущено
+                    (5, not "55") => "Вернуть результат 5", // второе значение не равно 5
+                    _ => throw new NotImplementedException() // default
+                };
+
+                // v4
+                var c = new Switch_();
+                var switchV4 = c switch
+                {
+                    { MyProperty: "1" } => "Вернуть результат 1",
+                    { MyProperty: "2" } => "Вернуть результат 2",
+                    _ => throw new NotImplementedException() // default
+                };
+            }
+        }
+
         class Collections_
         {
             static void Main_()
---
+++ README/FakeItEasy_.cs
@@ -0,0 +1,24 @@
+﻿// Создание фейкового объекта
+var foo = A.Fake<IFoo>();
+
+// Создание фейка-делегата
+var func = A.Fake<Func<string, int>>();
+
+// Создание коллекции фейков
+var foos = A.CollectionOfFake<Foo>(10);
+
+// Если тип фейка заранее неизвестен, можно использовать non-generic методы
+using FakeItEasy.Sdk;
+var type = GetTypeOfFake();
+object fake = Create.Fake(type);
+IList<object> fakes = Create.CollectionOfFake(type, 10);
+
+// Передача аргументов в фейковый конструктор, при этом настоящий конструктор не будет вызван
+// Способ 1
+var foo = A.Fake<FooClass>(x => x.WithArgumentsForConstructor(() => new FooClass("foo", "bar")));
+// Способ 2
+var foo = A.Fake<FooClass>(x => x.WithArgumentsForConstructor(new object[] { "foo", "bar" }));
+// Способ 3
+var foo = A.Fake<FooClass>(x => x.Implements<IFoo>());
+
+
