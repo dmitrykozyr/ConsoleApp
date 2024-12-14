9aed1a015ff95ff124b8f59e73887a0215bfe880
MYPC\dmitr <dmitry.kozyr.ua@gmail.com>
2024-12-14 09:30:25 +0300

    --

--- Projects/Architectures/LandscapePattersns/Microservices/Microservices.Products/Interfaces/IProductsProvider.cs
+++ Projects/Architectures/LandscapePattersns/Microservices/Microservices.Products/Interfaces/IProductsProvider.cs
@@ -5,7 +5,7 @@ namespace Microservices.Products.Interfaces
     public interface IProductsProvider
     {
         // Кортеж возвращает три значения
-        Task<(bool IsSuccess, IEnumerable<Models.Product> Products, string ErrorMessage)> GetProductsAsync();
+        Task<(bool IsSuccess, IEnumerable<Product> Products, string ErrorMessage)> GetProductsAsync();
         Task<(bool IsSuccess, Product Product, string ErrorMessage)> GetProductAsync(int id);
     }
 }
