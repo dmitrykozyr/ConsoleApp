e055b5f6a29c45c1384eafe2a7ba21fcc404de04
MYPC\dmitr <dmitry.kozyr.ua@gmail.com>
2025-02-09 23:24:51 +0300

    --

---
+++ Projects/API Gateway/Ocelot.Gateway/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Конфигурация Ocelot
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Конфигурация Ocelot
+builder.Services.AddOcelot(builder.Configuration);
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Конфигурация Ocelot
+await app.UseOcelot();
+
+await app.RunAsync();
---
+++ Projects/API Gateway/Yarp.Gateway/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.RateLimiting;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Конфигурация Yarp
+builder.Services.AddReverseProxy()
+                .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// Конфигурация балансировщика
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddPolicy("FixedWindow", context =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: context.Request.Path,
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                AutoReplenishment = true,
+                PermitLimit = 10,
+                Window = TimeSpan.FromSeconds(10)
+            }));
+});
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Конфигурация Yarp
+app.MapReverseProxy();
+
+// Конфигурация балансировщика
+app.UseRateLimiter();
+
+await app.RunAsync();
