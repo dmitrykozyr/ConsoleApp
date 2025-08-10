04cddc5d2c148c536d864f03e05966b2c67e64bb
MYPC\dmitr <dmitry.kozyr.ua@gmail.com>
2025-08-10 13:13:55 +0300

    --

---
+++ Projects/AuthenticationAuthorization/Keycloak/KeycloakWebAPI/Extensions/CustomExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace KeycloakWebAPI.Extensions;
+
+public static class CustomExtensions
+{
+    public static IServiceCollection AddSwaggerGenWithAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSwaggerGen(o =>
+        {
+            o.CustomSchemaIds(id => id.FullName!.Remove('+', '-'));
+
+            o.AddSecurityDefinition("Keycloak", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows
+                {
+                    Implicit = new OpenApiOAuthFlow
+                    {
+                        AuthorizationUrl = new Uri(configuration["Keycloak:AuthorizationUrl"]!),
+                        Scopes = new Dictionary<string, string>
+                        {
+                            { "openId", "openid" },
+                            { "profile", "profile" }
+                        }
+                    }
+                }
+            });
+
+            var securityRequirements = new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Keycloak",
+                            Type = ReferenceType.SecurityScheme
+                        },
+                        In = ParameterLocation.Header,
+                        Name = "Bearer",
+                        Scheme = "Bearer"
+                    },
+                    []
+                }
+            };
+
+            o.AddSecurityRequirement(securityRequirements);
+        });
+
+        return services;
+    }
+}
--- Projects/AuthenticationAuthorization/Keycloak/KeycloakWebAPI/Program.cs
+++ Projects/AuthenticationAuthorization/Keycloak/KeycloakWebAPI/Program.cs
@@ -1,19 +1,33 @@
+using KeycloakWebAPI.Extensions;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//!builder.Services.AddSwaggerGenWithAuth(builder.Configuration);
+//builder.Services.AddSwaggerGenWithAuth(builder.Configuration);
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
-//app.UseAuthentication();
+app.MapGet("users/me", (ClaimsPrincipal claimsPrincipal) =>
+{
+    return claimsPrincipal.Claims.ToDictionary(c => c.Type, c => c.Value);
+}).RequireAuthorization(); // Только пользователь с аутентификацией может вызвать этот эндпоинт
 
-//app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
