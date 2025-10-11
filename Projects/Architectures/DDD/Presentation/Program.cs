ec174b43a3f4d3be4a584cb8e8e4a6ca10d6a037
MYPC\dmitr <dmitry.kozyr.ua@gmail.com>
2025-10-11 10:15:01 +0300

    --

--- Projects/Architectures/DDD/Presentation/Program.cs
+++ Projects/Architectures/DDD/Presentation/Program.cs
@@ -1,34 +1,34 @@
 using Microsoft.FeatureManagement;
 using Presentation.Extensions;
 
-// Три способа настройки приложений
+// WebHost.CreateDefaultBuilder()
 /*
-    1. WebHost.CreateDefaultBuilder()
+    Создание веб-приложений на ASP.NET Core
+    Создает и настраивает IWebHostBuilder, отвечающий за настройку веб-сервера (например, Kestrel) и конфигурацию для веб-приложений
+    Автоматически настраивает параметры:
+    – чтение конфигурации из appsettings.json, переменных окружения и аргументов командной строки
+    – настройка логирования
+    – настройка Kestrel и других серверов
+    Используется для приложений ASP.NET Core до версии 6
+*/
 
-        • Создание веб-приложений на ASP.NET Core
-        • Создает и настраивает IWebHostBuilder, отвечающий за настройку веб-сервера (например, Kestrel) и конфигурацию для веб-приложений
-        • Автоматически настраивает параметры:
-          – чтение конфигурации из appsettings.json, переменных окружения и аргументов командной строки
-          – настройка логирования
-          – настройка Kestrel и других серверов
-        • Используется для приложений ASP.NET Core до версии 6
+// Host.CreateDefaultBuilder()
+/*
+    Создание общих приложений .NET (не обязательно веб)
+    Создает и настраивает IHostBuilder, который можно использовать для консольных и веб-приложений
+    Автоматически настраивает конфигурацию и логирование, но более универсален
+    Подходит для приложений, которые могут не включать веб-сервер
+    Рекомендуется использовать для создания фоновых служб, консольных и ASP.NET Core приложений, начиная с версии 3.0
+*/
 
-    2. Host.CreateDefaultBuilder()
-
-        • Создание общих приложений .NET (не обязательно веб)
-        • Создает и настраивает IHostBuilder, который можно использовать для консольных и веб-приложений
-        • Автоматически настраивает конфигурацию и логирование, но более универсален
-        • Подходит для приложений, которые могут не включать веб-сервер
-        • Рекомендуется использовать для создания фоновых служб, консольных и ASP.NET Core приложений, начиная с версии 3.0
-
-    3. WebApplication.CreateBuilder()
-
-        • Введен в ASP.NET Core 6 как упрощенный способ создания веб-приложений
-        • Создает WebApplicationBuilder, объединяеющий возможности Host и WebHost в одном подходе
-        • Позволяет добавлять службы, маршруты и middleware
-        • Автоматически включает конфигурацию и логирование, позволяет легко добавлять компоненты приложения в одном месте
-        • Рекомендуется использовать для новых проектов ASP.NET Core, так как этот подход более современный и упрощает процесс настройки приложения
-        • Предлагает более высокоуровневый API для настройки приложения, комбинируя функциональность обоих предыдущих методов
+// WebApplication.CreateBuilder()
+/*
+    Введен в ASP.NET Core 6 как упрощенный способ создания веб-приложений
+    Создает WebApplicationBuilder, объединяеющий возможности Host и WebHost в одном подходе
+    Позволяет добавлять службы, маршруты и middleware
+    Автоматически включает конфигурацию и логирование, позволяет легко добавлять компоненты приложения в одном месте
+    Рекомендуется использовать для новых проектов ASP.NET Core, так как этот подход более современный и упрощает процесс настройки приложения
+    Предлагает более высокоуровневый API для настройки приложения, комбинируя функциональность обоих предыдущих методов
 */
 
 var builder = WebApplication.CreateBuilder(args);
@@ -42,7 +42,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllersWithViews();
 builder.Services.AddFeatureManagement(builder.Configuration.GetSection("FeatureFlags"));
 
-// Методы расширения
 builder.Services.AddQuartzExtension(builder);
 builder.Services.AddRedisExtensions(builder);
 builder.Services.AddOptionsExtensions(builder);
@@ -54,7 +53,6 @@ IConfiguration configuration = builder.Services.AddConfigurationExtension(builde
 
 WebApplication app = builder.Build();
 
-// Сервисы, которым нужен WebApplication
 builder.Services.AddVaultExtensions(app, builder, configuration);
 
 if (!app.Environment.IsDevelopment())
